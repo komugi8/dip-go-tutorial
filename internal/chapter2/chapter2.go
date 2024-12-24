91856b7b40c28cbf31e3318f645fc66c01b912e0
komugi8 <8448eito@gmail.com>
2024-12-25 05:03:31 +0900

    chapter2: get api

--- internal/chapter2/chapter2.go
+++ internal/chapter2/chapter2.go
@@ -1,8 +1,46 @@
 package chapter2
 
-import "net/http"
-
-// revive:disable
+import (
+	"io"
+	"net/http"
+	"net/url"
+)
 
 func Get(w http.ResponseWriter, r *http.Request) {
+	u, err := url.Parse("http://mock:80/users")
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	q := u.Query()
+	q.Set("age", "25")
+	u.RawQuery = q.Encode()
+
+	req, err := http.NewRequest(http.MethodGet, u.String(), nil)
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	req.Header.Set("key", "dip")
+
+	client := http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	defer res.Body.Close()
+
+	w.Header().Set("Content-Type", "application/json")
+
+	_, err = io.Copy(w, res.Body)
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
 }
